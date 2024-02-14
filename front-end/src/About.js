80f059c0325f53f34d4e26232f54f8b29164b56f
ShadowCat567 <lv2067@nyu.edu>
2024-02-13 22:43:16 -0500

    Started writing about me

--- front-end/src/About.js
+++ front-end/src/About.js
@@ -4,7 +4,16 @@ const About = props => {
     return (
         <>
         <h1>About me</h1>
-        <p>Hello world</p>
+        <p>Hello! I am Leena. I am currently a senior at NYU, double majoring in
+         Computer Science and Game Design. Despite studying Game Design, I don't 
+         actually play very many games, so please don't come to me asking about 
+         recommendations. Of the games I do play, my favorites are currently Dungeons
+         and Dragons and Baldur's Gate 3.
+        </p>
+        <p>
+        I live with my cat Yin whose main hobbies are sleeping and attempting to 
+        climb to the highest location he can find.
+        </p>
         </>
     )
 }
